59c0302c238a16a305c5effbe86a1074199d8009
lihoujian <lihoujian@datagrand.com>
2024-07-17 17:19:34 +0800

    feat:layout布局组件引入和aside组件拆分

---
+++ src/pages/mall/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Mall = () => {
+    return (
+        <div>
+            <h1>Mall页面</h1>
+        </div>
+    )
+}
+
+export default Mall;
---
+++ src/pages/user/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+    return (
+        <div>
+            <h1>User页面</h1>
+        </div>
+    )
+}
+
+export default User;
